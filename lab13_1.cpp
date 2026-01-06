f0f5090c8514993504cc8ccd11a4556d1d85208c
khirakon <punratta111@gmail.com>
2026-01-06 15:49:10 +0700

    LAB13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,34 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    double sum = 0;
+    double sum_sq_diff = 0;
+    double log_sum = 0;
+    double recip_sum = 0;
+    double max_val = A[0];
+    double min_val = A[0];
+
+    for (int i = 0; i < N; i++) {
+        sum += A[i];
+        log_sum += log(A[i]);
+        recip_sum += 1.0 / A[i];
+        if (A[i] > max_val) max_val = A[i];
+        if (A[i] < min_val) min_val = A[i];
+    }
+
+ 
+    double mean = sum / N;
+    B[0] = mean;
+
+   
+    for (int i = 0; i < N; i++) {
+        sum_sq_diff += pow(A[i] - mean, 2);
+    }
+    B[1] = sqrt(sum_sq_diff / N);
+    B[2] = exp(log_sum / N);
+    B[3] = (double)N / recip_sum;
+    B[4] = max_val;
+    B[5] = min_val;
+}
